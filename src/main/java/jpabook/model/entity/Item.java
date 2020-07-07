bca1da5184de30e6cc3f12132578430f20d08784
chaehoon <chaehoon.leem@devmon.co.kr>
2020-07-07 13:14:28 +0900

    first add chapter 4.

---
+++ src/main/java/jpabook/model/entity/Item.java
@@ -0,0 +1,30 @@
+package jpabook.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Created by holyeye on 2014. 3. 11..
+ */
+
+@Entity
+@Table(name="ITEM")
+@Getter @Setter
+@ToString
+public class Item {
+
+    @Id @GeneratedValue
+    @Column(name = "ITEM_ID")
+    private Long id;
+
+    private String name;        //이름
+    private int price;          //가격
+    private int stockQuantity;  //재고수량
+}
---
+++ src/main/java/jpabook/model/entity/Member.java
@@ -0,0 +1,32 @@
+package jpabook.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Created by holyeye on 2014. 3. 11..
+ */
+
+@Entity
+@Table(name="MEMBER")
+@Getter @Setter
+@ToString
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+}
